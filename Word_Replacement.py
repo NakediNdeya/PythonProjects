00d246289569d47fd7f5c989d6e64dad87de06aa
NakediNdeya <ndeyanomalizo@gmail.com>
2023-06-12 14:35:51 +0200

    added Word replacement

---
+++ Word_Replacement.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str ="Hi guys ,i am Nakedi, and HI HI HI HI"
+    Word_to_replace = input("enter the word you want to replace: ")
+    Word_replacement = input("Enter the word replacement: ")
+    print(str.replace(Word_to_replace, Word_replacement))
+
+replace_word()
